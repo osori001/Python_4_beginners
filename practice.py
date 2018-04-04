f8d432b83f0bda4c3c77d354641becd24904f8b5
osori001 <5588183+osori001@users.noreply.github.com>
2018-04-04 14:23:41 -0700

    Update practice.py

--- practice.py
+++ practice.py
@@ -5,7 +5,7 @@ print ("Your name is "+name)
 """
 
 # 2. Odd or Even
-
+"""
 num = input("Give me a number: ")
 if num.isdigit() == True:
     if int(num) % 2 == 0:
@@ -15,5 +15,27 @@ if num.isdigit() == True:
     print ("Your number is "+oddeven)
 else:
     print ("aint a number")
-
+"""
 # 3. List Less Than Ten
+"""
+schindlers = []
+#####asks user for input to create a list.
+mten = 0
+while mten < 10:
+    num = input("Give me your number for index %d : " % (mten))
+    if num.isdigit() == True:
+        schindlers.append(int(num))
+        mten += 1
+    else:
+        print ("invalid input.")
+
+ltt = []
+#####filters initial inputs for less than ten.
+for s in range(0,10): #range(a,b) formatted from a to b-1
+    if schindlers[s] < 10:
+        ltt.append(schindlers[s])
+
+print (ltt)
+"""
+#4. Divisors
+
